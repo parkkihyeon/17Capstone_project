faf35344d308905005c9298312dba16da96d61a2
Seungsoo <nonestrike@gmail.com>
2017-11-28 03:51:23 -0800

    Node.js Fork to Cluster

--- src/CapstoneAIserver/app.js
+++ src/CapstoneAIserver/app.js
@@ -1,59 +1,85 @@
 #!/usr/bin/env node
 
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var cluster = require('cluster');
+cluster.schedulingPolicy = cluster.SCHED_RR;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
+var os = require('os');
+var redis = require('redis');
 
 //-----Connection Between Unity & Graph Module-----
-var Socket = require('socket.io');
-var socketEvents = require('./socket.js');
-const server = app.listen(6110, function () {
-    console.log("Unity Socket App on port 6110");
-});
+// var socketEvents = require('./socket.js');
+// // const server = app.listen(6110, function () {
+// //     //console.log("Unity Socket App on port 6110");
+// // });
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+// var net = require('net');
 
-const io = new Socket(server);
-const net = require('net');
+// server.listen(6000);
+var pub = redis.createClient(10000, "127.0.0.1");
+var sub = redis.createClient(10000, "127.0.0.1");
+var store = redis.createClient(10000, "127.0.0.1");
 
-socketEvents(io, net);
-//-------------------------------------------------
+if (cluster.isMaster) {
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+  //create worker as posible as the cpu can manage it
+  os.cpus().forEach(function (cpu) {
+        cluster.fork();
+  });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //if worker dead
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker exit : ' + worker.id);
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+    if (code == 200) {
+      cluster.fork();
+    }
+
+  });
+}
+else {
+  var app = express();
+  var socketEvents = require('./socket.js');
+  // const server = app.listen(6110, function () {
+  //     //console.log("Unity Socket App on port 6110");
+  // });
+  var server = require('http').Server(app);
+  var io = require('socket.io')(server);
+  var net = require('net');
+
+  server.listen(6000);
+
+  io.configure(function() {
+    io.set('log level', 1);
+    io.set('store', new io.RedisStore({
+      redis: redis,
+      redisPub: pub,
+      redisSubL: sub,
+      redisClient: store
+    }));
+  });
+
+  // socketEvents(io, net);
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+  });
+
+}
 
 module.exports = app;
